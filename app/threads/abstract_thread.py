d5a201dacd5fcde86d693ecf168f2fc8f1f814f6
metelitsaas <metelitsaas@gmail.com>
2021-05-09 01:04:06 +0300

    object_metadata property added to AbstractThread

--- app/threads/abstract_thread.py
+++ app/threads/abstract_thread.py
@@ -7,11 +7,14 @@ class AbstractThread(Thread, metaclass=ABCMeta):
     """
     Thread abstract class
     """
-    def __init__(self, shutdown_event: Event):
+    def __init__(self, object_metadata: dict, shutdown_event: Event):
         """
         Initialization
+        :param object_metadata: K8S object metadata
+        :param shutdown_event: event to shutdown thread
         """
-        Thread.__init__(self, daemon=True, target=self._run)
+        Thread.__init__(self, name=self.__class__.__name__, daemon=True, target=self._run)
+        self._object_metadata = object_metadata
         self._shutdown_event = shutdown_event
 
     def _run(self) -> None:
@@ -19,15 +22,15 @@ class AbstractThread(Thread, metaclass=ABCMeta):
         Run thread with exception handling
         """
         try:
-            logger.info(f"Starting thread")
+            logger.info("Starting thread")
             self._process()
 
         except Exception as error:
             logger.exception(error)
 
         finally:
+            logger.info("Stopping thread")
             self._shutdown_event.set()
-            self.join()
 
     @abstractmethod
     def _process(self) -> None:
