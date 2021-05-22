fc74b93828735729e4d3906430519c9b90134bab
metelitsaas <metelitsaas@gmail.com>
2021-05-23 00:06:38 +0300

    RTSPStreamOperator object's metadata renamed

--- app/rtsp_stream_operator.py
+++ app/rtsp_stream_operator.py
@@ -8,18 +8,18 @@ class RTSPStreamOperator:
     """
     RTSP Stream Operator
     """
-    def __init__(self, crd_group: str, crd_version: str, crd_plural: str, sleep_period: float = 5):
+    def __init__(self, group: str, version: str, plural: str, sleep_period: float = 5):
         """
         Initialization
-        :param crd_group: CustomResourceDefinition group name
-        :param crd_version: CustomResourceDefinition version
-        :param crd_plural: CustomResourceDefinition plural name
+        :param group: CustomResourceDefinition group name
+        :param version: CustomResourceDefinition version
+        :param plural: CustomResourceDefinition plural name
         :param sleep_period: sleep period between checks in seconds
         """
         self._object_metadata = {
-            'crd_group': crd_group,
-            'crd_version': crd_version,
-            'crd_plural': crd_plural
+            'group': group,
+            'version': version,
+            'plural': plural
         }
         self._sleep_period = sleep_period
         self._shutdown_event = Event()
